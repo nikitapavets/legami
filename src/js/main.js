47df757dc77c12453836288c95093b7c4d2e8e9d
Nikita Pavets <nikitapavets@gmail.com>
2017-08-02 14:25:41 +0300

    complete

--- src/js/main.js
+++ src/js/main.js
@@ -1,50 +1,16 @@
-class Slider {
-    constructor(sliderBox, leftArrow, rightArrow) {
-        this.sliderBox = sliderBox;
-        this.slides = sliderBox.children;
-        const slideStyles = this.slides[0].currentStyle || window.getComputedStyle(this.slides[0]);
-        this.slideWidth = this.slides[0].offsetWidth + parseInt(slideStyles.marginLeft) + parseInt(slideStyles.marginRight);
-        this.index = 0;
-        this.total = this.slides.length;
-        this.interval = 2000;
-        this.setArrowsEvents(leftArrow, rightArrow);   
-        this.init();
-    }
-
-    init() {
-        setInterval(() => { 
-            this.nextSlide();
-        }, this.interval);
-    }
-
-    rewind() {
-        this.index = 0;
-        this.sliderBox.style.left = 0; 
-    }
-
-    nextSlide() {
-        this.index++;
-        if(this.index == this.total - 1) {
-            this.rewind();
-            return;
-        }
-        let offset = this.sliderBox.offsetLeft - this.slideWidth;
-        this.sliderBox.style.left = offset + "px"; 
-    }
-
-    setArrowsEvents(leftArrow, rightArrow) {
-        if(leftArrow) {
-            rightArrow.addEventListener('click', this.nextSlide.bind(this), false);
-        }
-        if(rightArrow) {
-            rightArrow.addEventListener('click', this.nextSlide.bind(this), false);
-            this.index--;
-        }
-    }
-}
-
+import Slider from './slider';
 
 const sliderBox = document.querySelector('.slider');
 const leftArrow = document.querySelector('.slider--arrow.left');
 const rightArrow = document.querySelector('.slider--arrow.right');
 var slider = new Slider(sliderBox, leftArrow, rightArrow);
+
+// close popup
+document.getElementById('handleClosePopup').addEventListener('click', () => {
+    document.getElementById('popup').classList.add('hidden');
+}, false);
+
+// open popup
+document.getElementById('handlePersonalizza').addEventListener('click', () => {
+    document.getElementById('popup').classList.remove('hidden');
+}, false);
