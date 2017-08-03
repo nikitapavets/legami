6163dd7bdf039e93df4ee9f8f52bf2e952987be3
Nikita Pavets <nikitapavets@gmail.com>
2017-08-03 13:50:21 +0300

    fix cards

--- skin/frontend/legami/default/scripts/main.js
+++ skin/frontend/legami/default/scripts/main.js
@@ -15,6 +15,42 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById('handlePersonalizza').addEventListener('click', () => {
         document.getElementById('popup').classList.remove('hidden');
     }, false);
+
+    document.querySelector('.interactiveInput input').addEventListener('keyup', (event) => {
+        let interactiveInput = document.querySelector('.interactiveInput');
+        const maxLength = +interactiveInput.getAttribute('data-maxLength');
+        let interactiveCounter = interactiveInput.querySelector('.interactiveCounter');
+        interactiveCounter.innerHTML = `${event.target.value.length}/${maxLength}`;
+    }, false);
+
+    document.querySelector('.interactiveCards').addEventListener('click', (event) => {
+        if(event.target.getAttribute('value')) {
+            let cards = document.querySelectorAll('.interactiveCards li');
+            for(let i = 0; i < cards.length; i++){
+                if(cards[i].querySelector('div') == event.target) {
+                    cards[i].classList.add('active');
+                } else {
+                    cards[i].classList.remove('active');
+                }
+            }
+            document.querySelector('.interactiveCardValue').innerHTML = event.target.getAttribute('value');
+        }
+    }, false);
+
+
+    document.querySelector('.interactiveFontCards').addEventListener('click', (event) => {
+        if(event.target.getAttribute('value')) {
+            let cards = document.querySelectorAll('.interactiveFontCards li');
+            for(let i = 0; i < cards.length; i++){
+                if(cards[i].querySelector('div') == event.target) {
+                    cards[i].classList.add('active');
+                } else {
+                    cards[i].classList.remove('active');
+                }
+            }
+            document.querySelector('.interactiveFontCardValue').innerHTML = event.target.getAttribute('value');
+        }
+    }, false);
 });
 
 
